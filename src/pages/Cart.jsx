a597e43e4b0d298a48765c2ca9ed410cff46d32a
Raido Õunapuu <raidoounapuu@gmail.com>
2022-07-22 12:46:08 +0300

    homepage
    homepage + cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,20 @@
 
 function Cart() {
-  return ( <div>Ostukorv leht</div> );
-}
+  const [cart, setCart] = useState(JSON.parse(sessionStorage.getItem("cart")) || []);
+
+  return ( 
+    <div>
+      {cart.map(element => 
+        <div>
+          <div>{element.product.name}</div>
+          <div>{element.product.price}</div>
+          <button>-</button>
+          <div>{element.product.quantity}</div>
+          <button>+</button>
+          <div>{element.product.price * element.quantity} tk</div>
+          <button>x</button>
+        </div>)}
+    </div> );
+  }
 
 export default Cart;
